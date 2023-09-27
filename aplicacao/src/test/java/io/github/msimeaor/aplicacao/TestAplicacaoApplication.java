db8d655fb273bfea8afd30fc1eee4681c6049ca2
Matheus <maatsimeao@gmail.com>
2023-09-27 15:26:32 -0300

    Primeiro commit
    Primeiro commit

---
+++ aplicacao/src/test/java/io/github/msimeaor/aplicacao/TestAplicacaoApplication.java
@@ -0,0 +1,23 @@
+package io.github.msimeaor.aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestAplicacaoApplication {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(AplicacaoApplication::main).with(TestAplicacaoApplication.class).run(args);
+	}
+
+}
