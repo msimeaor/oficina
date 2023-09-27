ff264af4632ab276fea9c628c542e9c743b0d81c
Matheus <maatsimeao@gmail.com>
2023-09-27 15:48:57 -0300

    Mapeando entidade pessoa
    Mapeei a entidade pessoa para este POJO.

---
+++ aplicacao/src/main/java/io/github/msimeaor/aplicacao/model/entity/Pessoa.java
@@ -0,0 +1,46 @@
+package io.github.msimeaor.aplicacao.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private Long id;
+
+  @Column(name = "email", length = 100, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private boolean inativo;
+
+  @Column(name = "cpf", length = 14, unique = true)
+  private String cpf;
+
+  @Column(name = "sexo", length = 9, nullable = false)
+  private String sexo;
+
+  @Column(name = "nome", length = 50, nullable = false)
+  private String nome;
+
+  @Column(name = "data_nascimento")
+  private LocalDate dataNascimento;
+
+  //private Endereco endereco;
+  //private List<Telefone> telefones;
+  //private List<Venda> vendas;
+
+}
