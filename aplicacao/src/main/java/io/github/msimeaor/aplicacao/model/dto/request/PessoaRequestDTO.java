476d12197f7fae462e7779d2c8197f0740a4619f
Matheus <maatsimeao@gmail.com>
2023-09-28 16:54:13 -0300

    Validando PessoaRequestDTO
    Usei a API Jakarta.validation para validar os atributos do PessoaRequestDTO. Essas validações serão aplicadas em todos os objetos dessa classe que vierem em requisições.

--- aplicacao/src/main/java/io/github/msimeaor/aplicacao/model/dto/request/PessoaRequestDTO.java
+++ aplicacao/src/main/java/io/github/msimeaor/aplicacao/model/dto/request/PessoaRequestDTO.java
@@ -1,5 +1,9 @@
 package io.github.msimeaor.aplicacao.model.dto.request;
 
+import io.github.msimeaor.aplicacao.helper.EmailValidationGroup;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +17,20 @@ import java.util.List;
 @Builder
 public class PessoaRequestDTO {
 
+  @NotBlank(message = "{error.message.notblank}")
+  @Size(max = 50, message = "{error.message.size.limit}")
   private String nome;
+
+  @Email(groups = EmailValidationGroup.class)
   private String email;
+
+  @Size(max = 14, message = "{error.message.invalid.cpf}")
   private String cpf;
+
+  @NotBlank(message = "{error.message.notblank}")
+  @Size(max = 9, message = "{error.message.invalid.sexo}")
   private String sexo;
+
   private List<Long> telefones;
   private Long endereco;
 
