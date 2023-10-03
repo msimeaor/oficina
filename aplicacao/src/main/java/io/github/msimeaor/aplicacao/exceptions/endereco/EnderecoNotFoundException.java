a1a8c990c605f5ba2bc8aad4768d5fab6e545ba2
Matheus <maatsimeao@gmail.com>
2023-10-03 15:37:13 -0300

    Tratando exceção de EnderecoNotFoundException
    Criei a exception personalizada e adicionei ao controller das ExceptionHandler que vai capturar a exception e devolver no body das responses com uma formatação melhor.

---
+++ aplicacao/src/main/java/io/github/msimeaor/aplicacao/exceptions/endereco/EnderecoNotFoundException.java
@@ -0,0 +1,9 @@
+package io.github.msimeaor.aplicacao.exceptions.endereco;
+
+public class EnderecoNotFoundException extends RuntimeException {
+
+  public EnderecoNotFoundException(String message) {
+    super(message);
+  }
+
+}
