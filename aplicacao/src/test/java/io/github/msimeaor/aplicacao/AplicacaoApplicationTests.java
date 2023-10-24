2f92abf651bef6a1802ada3bb5f3edc278e093d5
Matheus <maatsimeao@gmail.com>
2023-10-24 16:56:05 -0300

    Some little adjusts
    Doing some little adjusts

--- aplicacao/src/test/java/io/github/msimeaor/aplicacao/AplicacaoApplicationTests.java
+++ aplicacao/src/test/java/io/github/msimeaor/aplicacao/AplicacaoApplicationTests.java
@@ -1,10 +1,11 @@
 package io.github.msimeaor.aplicacao;
 
+import io.github.msimeaor.aplicacao.integration.testcontainer.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AplicacaoApplicationTests {
+class AplicacaoApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
