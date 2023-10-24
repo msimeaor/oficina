4138e5833b2609ce5a9c8f886a4c069e5cea5315
Matheus <maatsimeao@gmail.com>
2023-10-24 11:04:48 -0300

    Creating the save method integration test passing the id in enderecoId
    - I created the save method integration test in PessoaRestController passing the id in enderecoId
    - I changed the way to instantiate a PessoaRequestDTOTest, now using a builder method

--- aplicacao/src/test/java/io/github/msimeaor/aplicacao/integration/controller/PessoaRestControllerTest.java
+++ aplicacao/src/test/java/io/github/msimeaor/aplicacao/integration/controller/PessoaRestControllerTest.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.msimeaor.aplicacao.config.TestConfigs;
+import io.github.msimeaor.aplicacao.enums.UFs;
 import io.github.msimeaor.aplicacao.integration.dto.request.PessoaRequestDTOTest;
 import io.github.msimeaor.aplicacao.integration.dto.response.PessoaResponseDTOTest;
 import io.github.msimeaor.aplicacao.integration.testcontainer.AbstractIntegrationTest;
+import io.github.msimeaor.aplicacao.model.dto.response.EnderecoResponseDTO;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
@@ -25,16 +27,13 @@ public class PessoaRestControllerTest extends AbstractIntegrationTest {
   private static RequestSpecification specification;
   private static ObjectMapper mapper;
   private static PessoaRequestDTOTest pessoaRequestDTOTest;
-  private static PessoaResponseDTOTest pessoaResponseDTOTest;
+  private static PessoaRequestDTOTest pessoaRequestDTOTestWithPessoaId;
 
   @BeforeAll
   public static void setup() {
     mapper = new ObjectMapper();
     mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     mapper.findAndRegisterModules();
-
-    pessoaRequestDTOTest = new PessoaRequestDTOTest();
-    pessoaResponseDTOTest = new PessoaResponseDTOTest();
   }
 
   @Test
@@ -60,26 +59,77 @@ public class PessoaRestControllerTest extends AbstractIntegrationTest {
               .body()
                 .asString();
 
-    pessoaResponseDTOTest = mapper.readValue(content, PessoaResponseDTOTest.class);
+    var pessoaResponseDTO = mapper.readValue(content, PessoaResponseDTOTest.class);
 
-    assertNotNull(pessoaResponseDTOTest);
+    assertNotNull(pessoaResponseDTO);
     // A migration containing 50 records was applied. This record is number 51.
-    assertEquals(51L, pessoaResponseDTOTest.getId());
-    assertEquals(pessoaRequestDTOTest.getNome(), pessoaResponseDTOTest.getNome());
-    assertEquals(pessoaRequestDTOTest.getCpf(), pessoaResponseDTOTest.getCpf());
-    assertEquals(pessoaRequestDTOTest.getEmail(), pessoaResponseDTOTest.getEmail());
-    assertEquals(pessoaRequestDTOTest.getSexo(), pessoaResponseDTOTest.getSexo());
-    assertEquals(pessoaRequestDTOTest.getDataNascimento(), pessoaResponseDTOTest.getDataNascimento());
+    assertEquals(51L, pessoaResponseDTO.getId());
+    assertEquals(pessoaRequestDTOTest.getNome(), pessoaResponseDTO.getNome());
+    assertEquals(pessoaRequestDTOTest.getCpf(), pessoaResponseDTO.getCpf());
+    assertEquals(pessoaRequestDTOTest.getEmail(), pessoaResponseDTO.getEmail());
+    assertEquals(pessoaRequestDTOTest.getSexo(), pessoaResponseDTO.getSexo());
+    assertEquals(pessoaRequestDTOTest.getDataNascimento(), pessoaResponseDTO.getDataNascimento());
+    assertNull(pessoaResponseDTO.getEnderecoResponse());
 
     assertTrue(content.contains("{\"self\":{\"href\":\"http://localhost:8888/api/pessoas/51\"}}"));
   }
 
+  @Test
+  @Order(1)
+  public void savePersonWithPessoaIdWithSuccess() throws JsonProcessingException {
+    mockPessoaWithPessoaId();
+
+    var content = given().spec(specification)
+            .basePath("/api/pessoas")
+            .body(pessoaRequestDTOTestWithPessoaId)
+            .pathParam("placa", "JJJ1111")
+            .when()
+              .post("{placa}")
+            .then()
+              .statusCode(201)
+            .extract()
+              .body()
+                .asString();
+
+    var pessoaResponseDTO = mapper.readValue(content, PessoaResponseDTOTest.class);
+
+    assertNotNull(pessoaResponseDTO);
+    // Take into account that the 51 ID was filled in the first test
+    assertEquals(52L, pessoaResponseDTO.getId());
+    assertEquals(pessoaRequestDTOTestWithPessoaId.getNome(), pessoaResponseDTO.getNome());
+    assertEquals(pessoaRequestDTOTestWithPessoaId.getCpf(), pessoaResponseDTO.getCpf());
+    assertEquals(pessoaRequestDTOTestWithPessoaId.getEmail(), pessoaResponseDTO.getEmail());
+    assertEquals(pessoaRequestDTOTestWithPessoaId.getSexo(), pessoaResponseDTO.getSexo());
+    assertEquals(pessoaRequestDTOTestWithPessoaId.getDataNascimento(), pessoaResponseDTO.getDataNascimento());
+
+    assertNotNull(pessoaResponseDTO.getEnderecoResponse());
+    assertEquals(EnderecoResponseDTO.class, pessoaResponseDTO.getEnderecoResponse().getClass());
+    assertEquals(1L, pessoaResponseDTO.getEnderecoResponse().getId());
+    assertEquals("68643 Shopko Hill", pessoaResponseDTO.getEnderecoResponse().getLogradouro());
+    assertEquals(UFs.DF, pessoaResponseDTO.getEnderecoResponse().getUf());
+
+    assertTrue(content.contains("{\"self\":{\"href\":\"http://localhost:8888/api/pessoas/52\"}}"));
+  }
+
   public void mockPessoa() {
-    pessoaRequestDTOTest.setNome("MATHEUS SIMEAO");
-    pessoaRequestDTOTest.setCpf("000.000.000-00");
-    pessoaRequestDTOTest.setSexo("MASCULINO");
-    pessoaRequestDTOTest.setEmail("maatsimeao@gmail.com");
-    pessoaRequestDTOTest.setDataNascimento(LocalDate.of(2002, 05, 11));
+    pessoaRequestDTOTest = PessoaRequestDTOTest.builder()
+            .nome("MATHEUS SIMEAO")
+            .cpf("000.000.000-00")
+            .sexo("MASCULINO")
+            .email("maatsimeao@gmail.com")
+            .dataNascimento(LocalDate.of(2002, 05, 11))
+            .build();
+  }
+
+  public void mockPessoaWithPessoaId() {
+    pessoaRequestDTOTestWithPessoaId = PessoaRequestDTOTest.builder()
+            .nome("ROGERIO MADURO")
+            .cpf("999.999.999-99")
+            .sexo("FEMININO")
+            .email("rogeriomaduro@gmail.com")
+            .dataNascimento(LocalDate.of(2002, 05, 11))
+            .enderecoId(1L)
+            .build();
   }
 
 }
