99d20fc770b73dc90bd3e33f24164ca1f20f3497
Matheus <maatsimeao@gmail.com>
2023-10-26 11:16:55 -0300

    Creating save methods integration tests
    - I created two DTO class to be passed in body of requests and responses.
    - I created a test of save method without an person list and a method test by passing this list in request body

---
+++ aplicacao/src/test/java/io/github/msimeaor/aplicacao/integration/controller/EnderecoRestControllerTest.java
@@ -0,0 +1,124 @@
+package io.github.msimeaor.aplicacao.integration.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.msimeaor.aplicacao.config.TestConfigs;
+import io.github.msimeaor.aplicacao.enums.UFs;
+import io.github.msimeaor.aplicacao.integration.dto.request.EnderecoRequestDTOTest;
+import io.github.msimeaor.aplicacao.integration.dto.response.EnderecoResponseDTOTest;
+import io.github.msimeaor.aplicacao.integration.testcontainer.AbstractIntegrationTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import java.util.Collections;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EnderecoRestControllerTest extends AbstractIntegrationTest {
+
+  private static RequestSpecification specification;
+  private static ObjectMapper mapper;
+  private static EnderecoRequestDTOTest enderecoRequestDTOTest;
+  private static EnderecoRequestDTOTest enderecoRequestDTOTestWithPersonIdList;
+  private static EnderecoResponseDTOTest enderecoResponseDTOTest;
+
+  @BeforeAll
+  public static void setup() {
+    mapper = new ObjectMapper();
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+    startEntities();
+  }
+
+  @Test
+  @Order(0)
+  public void saveWithoutPessoaIdList() throws JsonProcessingException {
+    specification = new RequestSpecBuilder()
+            .setPort(TestConfigs.SERVER_PORT)
+            .addHeader(TestConfigs.ORIGIN, "http://localhost:8080")
+            .setContentType(MediaType.APPLICATION_JSON_VALUE)
+            .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+            .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+            .build();
+
+    var content = given().spec(specification)
+            .basePath("/api/enderecos")
+            .body(enderecoRequestDTOTest)
+            .when()
+              .post()
+            .then()
+              .statusCode(201)
+            .extract()
+              .body()
+                .asString();
+
+    var enderecoResponseDTO = mapper.readValue(content, EnderecoResponseDTOTest.class);
+
+    assertNotNull(enderecoResponseDTO);
+    /*
+    In this case, was applied a migration in database containing ten address records. When adding a new record,
+    then it will have the eleventh ID
+    */
+    assertEquals(11L, enderecoResponseDTO.getId());
+    assertEquals("Area Especial 2A Modulo E Lote 5", enderecoResponseDTO.getLogradouro());
+    assertEquals(UFs.DF, enderecoResponseDTO.getUf());
+    /*
+    Take into account that no person ids were added to the list of person ids in EnderecoRequestDTO.
+    */
+    assertTrue(content.contains("\"_links\":{\"self\":{\"href\":\"http://localhost:8888/api/enderecos/11\"}}"));
+  }
+
+  @Test
+  @Order(1)
+  public void saveWithPessoaIdList() throws JsonProcessingException {
+    var content = given().spec(specification)
+            .basePath("/api/enderecos")
+            .body(enderecoRequestDTOTestWithPersonIdList)
+            .when()
+              .post()
+            .then()
+              .statusCode(201)
+            .extract()
+              .body()
+                .asString();
+
+    var enderecoResponseDTO = mapper.readValue(content, EnderecoResponseDTOTest.class);
+
+    assertNotNull(enderecoResponseDTO);
+    assertEquals(12L, enderecoResponseDTO.getId());
+    assertEquals("QNP 15 Conjunto I", enderecoResponseDTO.getLogradouro());
+    assertEquals(UFs.DF, enderecoResponseDTO.getUf());
+
+    /*
+    Now, as we passed a person id to request, the response body will return this HATEOAS link containing
+    this request path to Pessoa controller.
+    */
+    assertTrue(content.contains("\"Morador(es)\":{\"href\":\"http://localhost:8888/api/pessoas/8\"}"));
+    assertTrue(content.contains("\"self\":{\"href\":\"http://localhost:8888/api/enderecos/12\"}"));
+  }
+
+  public static void startEntities() {
+    enderecoRequestDTOTest = EnderecoRequestDTOTest.builder()
+            .logradouro("Area Especial 2A Modulo E Lote 5")
+            .uf(UFs.DF)
+            .pessoasId(null)
+            .build();
+
+    enderecoRequestDTOTestWithPersonIdList = EnderecoRequestDTOTest.builder()
+            .logradouro("QNP 15 Conjunto I")
+            .uf(UFs.DF)
+            .pessoasId(Collections.singletonList(8L))
+            .build();
+  }
+
+}
