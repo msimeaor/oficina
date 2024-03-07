506a8e81a441b7ba70f28cad74e2ff88a42cf49a
Matheus <maatsimeao@gmail.com>
2024-03-07 11:15:49 -0300

    Creating the save endpoint documentation of the VeiculoRestController
    I created the endpoint documentation. It explains how to do the request and how the API return the responses.

--- aplicacao/src/main/java/io/github/msimeaor/aplicacao/controller/VeiculoRestController.java
+++ aplicacao/src/main/java/io/github/msimeaor/aplicacao/controller/VeiculoRestController.java
@@ -1,9 +1,16 @@
 package io.github.msimeaor.aplicacao.controller;
 
+import io.github.msimeaor.aplicacao.exceptions.ExceptionResponse;
 import io.github.msimeaor.aplicacao.model.dto.request.VeiculoRequestDTO;
 import io.github.msimeaor.aplicacao.model.dto.response.VeiculoResponseDTO;
 import io.github.msimeaor.aplicacao.model.service.impl.VeiculoServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/veiculos")
+@Tag(name = "Veiculo Rest Controller")
 public class VeiculoRestController {
 
   private VeiculoServiceImpl service;
@@ -20,6 +28,31 @@ public class VeiculoRestController {
     this.service = service;
   }
 
+  @Operation(summary = "Save a vehicle in database", description = "Save a vehicle in database",
+    tags = {"Save"},
+    responses = {
+      @ApiResponse(description = "Success", responseCode = "201",
+        content = {
+          @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = VeiculoResponseDTO.class)
+          )
+        }
+      ),
+      @ApiResponse(description = "Vehicle with this license plate already registered", responseCode = "409",
+        content = {
+          @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = ExceptionResponse.class)
+          )
+        }
+      ),
+      @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+      @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+      @ApiResponse(description = "Forbiden", responseCode = "403", content = @Content),
+      @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    }
+  )
   @PostMapping()
   public ResponseEntity<VeiculoResponseDTO> save(@RequestBody @Valid VeiculoRequestDTO veiculoRequestDTO) {
     return service.save(veiculoRequestDTO);
